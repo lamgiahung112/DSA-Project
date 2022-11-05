77c2880184b9fdff2e5a270b9270aa764b4e4856
Hung <Lam>
2022-11-05 17:01:35 +0700

    setup

---
+++ README.txt
@@ -0,0 +1,7 @@
+Format của file input:
+
+- n dòng
+- mỗi dòng: [x] [y] [z]
+- x: tên của đỉnh đầu của đoạn
+- y: tên của đỉnh cuối của đoạn
+- z: trọng số của đoạn
--- main.cpp
+++ main.cpp
@@ -1,13 +1,116 @@
-#include "Game.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    string name;
+    vector<Node*> neighbors;
+};
+
+map<string, map<string, int>> weightage; // weightage["A"]["B"]
+vector<Node*> nodes;
+
+/**
+ * @brief Đọc file và lưu vào các biến
+ * 
+ */
+void init() {};
+
+/**
+ * @brief Lưu weightage và nodes vào file input.txt
+ * 
+ */
+void saveToFile() {};
+
+/**
+ * @brief Thêm node
+ * 
+ */
+void addNode() {};
+
+/**
+ * @brief Sửa node / trọng số
+ * 
+ */
+void editNode() {};
+
+/**
+ * @brief Xóa node
+ * 
+ */
+void deleteNode() {};
+
+/**
+ * @brief Demo thuật toán DFS
+ * 
+ */
+void showDFS() {};
+
+/**
+ * @brief Demo thuật toán BFS
+ * 
+ */
+void showBFS() {};
+
+/**
+ * @brief Demo đường đi ngắn nhất giữa 2 điểm
+ * 
+ */
+void showShortestPath() {};
+
+/**
+ * @brief Demo các phần liên thông của đồ thị, show các đỉnh của thành phần liên thông
+ * 
+ */
+void showThanhPhanLienThong() {};
+
+/**
+ * @brief Demo chu trình hamilton
+ * 
+ */
+void showHamilton() {
+
+}
+
+/**
+ * @brief Demo chu trình Euler
+ * 
+ */
+void showEuler() {
+
+}
+
+/**
+ * @brief Demo đỉnh trụ của đồ thị
+ * 
+ */
+void showDinhTru() {};
+
+/**
+ * @brief Demo đỉnh thắt của đồ thị
+ * 
+ */
+void showDinhThat() {};
+
+/**
+ * @brief Demo cạnh cầu của đồ thị
+ * 
+ */
+void showCanhCau() {};
+
+/**
+ * @brief Demo thuật toán topological sort
+ * 
+ */
+void showTopologicalSort() {};
+
+/**
+ * @brief Demo bài tô màu đồ thị
+ * 
+ */
+void showGraphColoring() {};
 
 int main() {
-    Game game;
-
-    while (game.isRunning()) {
-        game.update();
-
-        game.render();
-    }
-
+    
     return 0;
 }
