0c489b33a749b0672fcb66ed376f9e121f342f1d
Hung <Lam>
2022-11-13 13:53:54 +0700

    back button and next button

--- browser-script.js
+++ browser-script.js
@@ -1,9 +1,71 @@
+const BrowserHistory = function () {
+	this.page = {
+		url: homeURL,
+		back: null,
+		next: null,
+	}
+}
+
+BrowserHistory.prototype.visit = function (url) {
+	this.page.next = {
+		url,
+		back: this.page,
+		next: null,
+	}
+	this.page = this.page.next
+}
+
+BrowserHistory.prototype.back = function () {
+	if (!this.page.back) return
+	this.page = this.page.back
+	return this.page.url
+}
+
+BrowserHistory.prototype.next = function () {
+	if (!this.page.next) return
+	this.page = this.page.next
+	return this.page.url
+}
+
 const byId = (id) => {
 	return document.getElementById(id)
 }
 
-const view = byId("webview")
+const homeURL = "https://www.google.com"
+const browserHistory = new BrowserHistory()
+var isVisit = true
 
-view.addEventListener("load-commit", () => {
-	byId("url-input").value = view.getURL()
+const view = byId("webview")
+const backBtn = byId("back-btn")
+const nextBtn = byId("next-btn")
+const reloadBtn = byId("reload-btn")
+const urlInput = byId("url-input")
+const historyBtn = byId("history-btn")
+const bookmarkBtn = byId("bookmark-btn")
+const homeBtn = byId("home-btn")
+const console = byId("console")
+
+view.addEventListener("did-finish-load", () => {
+	if (isVisit) browserHistory.visit(view.getURL())
+	urlInput.value = view.getURL()
 })
+
+backBtn.addEventListener("click", () => {
+	const backURL = browserHistory.back()
+	if (!backURL) return
+	isVisit = false
+	view.loadURL(backURL)
+})
+
+nextBtn.addEventListener("click", () => {
+	const nextURL = browserHistory.next()
+	if (!nextURL) return
+	isVisit = false
+	view.loadURL(nextURL)
+})
+
+homeBtn.addEventListener("click", () => {
+	view.loadURL(homeURL)
+})
+
+reloadBtn.addEventListener("click", () => {})
