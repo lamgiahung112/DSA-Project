3ffacfcd3ed1fe6b99a66ce4ff9a315716ce7ad8
Hung <Lam>
2022-11-14 19:57:25 +0700

    fix history

--- browser-script.js
+++ browser-script.js
@@ -1,3 +1,23 @@
+class Stack {
+	constructor() {
+		this.items = []
+	}
+
+	push(element) {
+		this.items.push(element)
+	}
+
+	top() {
+		if (this.items.length == 0) return null
+		return this.items[this.items.length - 1]
+	}
+
+	pop() {
+		if (this.items.length === 0) return
+		this.items.pop()
+	}
+}
+
 const BrowserHistory = function () {
 	this.page = {
 		url: homeURL,
@@ -31,8 +51,13 @@ const byId = (id) => {
 	return document.getElementById(id)
 }
 
+const fs = require("fs")
+
 const homeURL = "https://www.google.com"
 const browserHistory = new BrowserHistory()
+const historyStack = new Stack()
+historyStack.items = require("./history.json")
+
 var isVisit = true
 
 const view = byId("webview")
@@ -43,9 +68,6 @@ const urlInput = byId("url-input")
 const historyBtn = byId("history-btn")
 const bookmarkBtn = byId("bookmark-btn")
 const homeBtn = byId("home-btn")
-const console = byId("console")
-const stackHistory = new Stack()
-
 
 view.addEventListener("did-finish-load", () => {
 	if (isVisit) {
@@ -53,14 +75,11 @@ view.addEventListener("did-finish-load", () => {
 	}
 	isVisit = true
 	urlInput.value = view.getURL()
-	var obj = []
-	obj.push(view.getURL())//add url to history.json, chỗ này thêm cái tab name nữa là hợp lí
-	const fs = require('fs')
-	fs.writeFile('testJSON.json', JSON.stringify(obj), err => {
-		if (err) {
-		  throw err
-		}
-	  })
+
+	historyStack.push(view.getURL())
+	fs.writeFile("./history.json", JSON.stringify(historyStack.items), (err) => {
+		homeBtn.innerText = err.message
+	})
 })
 
 backBtn.addEventListener("click", () => {
@@ -82,21 +101,3 @@ homeBtn.addEventListener("click", () => {
 })
 
 reloadBtn.addEventListener("click", () => {})
-
-class Stack{
-    constructor() {
-        this.items = [];
-        this.count = 0;
-    }
-    push(element){
-        this.items[this.count++] = element;     
-    }
-    
-    top(){
-        if(this.count == 0) return undefined
-        let deleteItem = this.items[this.count - 1];
-    }
-}
-
-
-
