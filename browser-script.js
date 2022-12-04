0c81525260114268c16b2297606d52b46a9bfbf1
Hung <Lam>
2022-12-04 20:02:47 +0700

    tab logic

--- browser-script.js
+++ browser-script.js
@@ -101,14 +101,19 @@ const browserHistory = new BrowserHistory()
 const historyStack = new Stack()
 const bookmarkStack = new Stack()
 
+const tabMap = new Map()
 ;(() => {
 	require("./history.json").forEach((item) => historyStack.push(item))
-	require("./bookmark.json").forEach((item) => bookmarkStack.push(item))
+	// require("./bookmark.json").forEach((item) => bookmarkStack.push(item))
 })()
 
 var isVisit = true
 
-const view = byId("webview")
+let currentTabid
+const webviewContainer = byId("webview-container")
+const addTabBtn = byId("btn-new-tab")
+const tabRenderer = byId("tab-renderer")
+
 const backBtn = byId("back-btn")
 const nextBtn = byId("next-btn")
 const reloadBtn = byId("reload-btn")
@@ -117,12 +122,18 @@ const historyBtn = byId("history-btn")
 const bookmarkBtn = byId("bookmark-btn")
 const homeBtn = byId("home-btn")
 
-view.addEventListener("did-finish-load", () => {
+/** RENDER WEBVIEW */
+
+/** WEBVIEW EVENT HANDLERS */
+
+const tabEventHandler = (id) => {
+	const view = tabMap.get(id)
 	if (isVisit) {
 		browserHistory.visit(view.getURL())
 	}
 	isVisit = true
 	urlInput.value = view.getURL()
+	byId(`tab-title-${currentTabid}`).textContent = view.getTitle()
 
 	historyStack.push({
 		url: view.getURL(),
@@ -134,7 +145,88 @@ view.addEventListener("did-finish-load", () => {
 		if (err) console.log(err)
 	})
 	console.log(historyStack.getList())
+}
+
+const resetTabEventHandler = () => {
+	for (const id of tabMap.keys()) {
+		const view = tabMap.get(id)
+
+		view.removeEventListener("did-finish-load", () => tabEventHandler(id))
+		view.addEventListener("did-finish-load", () => tabEventHandler(id))
+	}
+}
+
+resetTabEventHandler()
+
+addTabBtn.addEventListener("click", () => {
+	for (const key of tabMap.keys()) {
+		tabMap.get(key).style.width = "0"
+		tabMap.get(key).style.height = "0"
+	}
+
+	const newWebview = document.createElement("webview")
+	const id = randomUUID()
+	newWebview.setAttribute("id", id)
+	newWebview.setAttribute("src", "https://www.google.com")
+	newWebview.setAttribute("webpreferences", "contextIsolation=false")
+	newWebview.textContent = " "
+	webviewContainer.appendChild(newWebview)
+	tabMap.set(id, byId(id))
+	currentTabid = id
+
+	const tabToAdd = document.createElement("div")
+	tabToAdd.className = "chrome-tab"
+	tabToAdd.toggleAttribute("active")
+
+	const content = document.createElement("div")
+	content.className = "chrome-tab-content"
+
+	const tabTitle = document.createElement("div")
+	tabTitle.className = "chrome-tab-title"
+	tabTitle.id = `tab-title-${id}`
+
+	const tabClose = document.createElement("div")
+	tabClose.className = "chrome-tab-close"
+	tabClose.id = `tab-close-${id}`
+
+	tabClose.addEventListener("click", () => {
+		tabRenderer.removeChild(tabToAdd)
+		webviewContainer.removeChild(newWebview)
+		tabMap.delete(id)
+		tabMap.forEach((_, key) => (currentTabid = key))
+
+		if (currentTabid !== id) {
+			tabMap.get(currentTabid).style.width = "100%"
+			tabMap.get(currentTabid).style.height = "680px"
+			urlInput.value = tabMap.get(currentTabid).getURL()
+		} else {
+			urlInput.value = ""
+		}
+	})
+
+	tabToAdd.addEventListener("click", () => {
+		if (currentTabid == id) return
+		currentTabid = id
+		tabMap.forEach((webview) => {
+			webview.style.width = "0"
+			webview.style.height = "0"
+		})
+		tabMap.get(currentTabid).style.width = "100%"
+		tabMap.get(currentTabid).style.height = "680px"
+		urlInput.value = tabMap.get(currentTabid).getURL()
+	})
+
+	content.appendChild(tabTitle)
+	content.appendChild(tabClose)
+
+	tabToAdd.innerHTML = chromeTabBackground
+	tabToAdd.appendChild(content)
+
+	tabRenderer.appendChild(tabToAdd)
+	resetTabEventHandler()
 })
+
+/**OTHER EVENTS */
 
 backBtn.addEventListener("click", () => {
 	const backURL = browserHistory.back()
@@ -170,3 +262,7 @@ historyBtn.addEventListener("click", () => {
 	urlInput.value = "browser://history"
 	view.loadURL("./history.html")
 })
+
+const chromeTabBackground = `<div class="chrome-tab-background">
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol viewBox="0 0 214 36"><path d="M17 0h197v36H0v-2c4.5 0 9-3.5 9-8V8c0-4.5 3.5-8 8-8z"/></symbol><symbol id="chrome-tab-geometry-right" viewBox="0 0 214 36"><use xlink:href="#chrome-tab-geometry-left"/></symbol><clipPath id="crop"><rect class="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="52%" height="100%"><use xlink:href="#chrome-tab-geometry-left" width="214" height="36" class="chrome-tab-geometry"/></svg><g transform="scale(-1, 1)"><svg width="52%" height="100%" x="-100%" y="0"><use xlink:href="#chrome-tab-geometry-right" width="214" height="36" class="chrome-tab-geometry"/></svg></g></svg>
+</div>`
